3fca26252a2ff700a80317198b0422359db206c5
Reda TRANKIL <reda.trankil@gmail.com>
2026-03-27 10:33:21 +0100

    add sidebar in data page

--- R/modules/data_module.R
+++ R/modules/data_module.R
@@ -3,7 +3,32 @@
 data_ui <- function(id) {
   ns <- NS(id)
   
-  tags$h2("Data Page - Under Construction", class = "page-title")
+  tagList(
+    tags$head(
+      tags$style(rel = "stylesheet", type = "text/css", href = "css/data_module.css")
+    ),
+    
+    # div of the config panel
+    div(
+      class = "config-panel-wrapper",
+      
+      # ── Left sidebar ──────────────────────────────
+      div(
+        class = "config-sidebar",
+        
+        div(class = "title-data", tags$p("Data indices")),
+      
+        div(class = "title-descr", tags$p("Sélectionnez un indice pour consulter la documentation."))
+      ),
+      
+      # ── Right panel (doc viewer placeholder) ──────
+      div(
+        class = "doc-panel",
+        style = "flex:1; padding: 40px;"
+        
+      )
+    )
+  )
 }
 
 data_server <- function(id) {
